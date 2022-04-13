27675556b39d9c3bab61db0b575446d95e917d2d
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-13 21:14:46 +0600

    login , register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import ServicesDetails from './Pages/Home/ServiceDetails/ServicesDetails';
+import Login from './Pages/login/Login';
+import Register from './Pages/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import Notfound from './Pages/Shared/NotFound/Notfound';
@@ -17,6 +19,8 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<ServicesDetails></ServicesDetails>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<Notfound></Notfound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate()
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
+
+        console.log(email, password);
+    }
+
+    const navigateRegister = () => {
+        navigate('/register')
+    }
+
+    return (
+        <div className='mx-auto container row' >
+            <h2 className='text-center my-4'>Please Login </h2>
+            <div className='col-md-4 col-12'>
+
+            </div>
+            <Form onSubmit={handleSubmit} className='col-md-4 col-12'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+
+                <p className='mt-1'>New to Genius Car ? <span onClick={navigateRegister} className='text-info underline'>Please Register Now </span></p>
+            </Form>
+
+
+            <div className='col-md-4 col-12'>
+
+            </div>
         </div>
     );
 };
