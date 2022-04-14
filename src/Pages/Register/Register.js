4495fa9b9d246cb78f55405129ce009ddaaf0e1f
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-14 17:43:38 +0600

    Email password based authentication with react firebase hooks

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,18 +1,31 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate()
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const nameRef = useRef('');
+
 
     const navigateLogin = event => {
         navigate('/login')
     }
     const handleRegister = event => {
         event.preventDefault()
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
+        console.log(createUserWithEmailAndPassword());
     }
     return (
         <div className='mx-auto container row' >
@@ -23,17 +36,17 @@ const Register = () => {
             <Form onSubmit={handleRegister} className='col-md-4 col-12'>
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Your Name</Form.Label>
-                    <Form.Control ref={nameRef} type="text" placeholder="Your Name" />
+                    <Form.Control type="text" placeholder="Your Name" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
